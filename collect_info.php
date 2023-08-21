ed4d64db716791faed3f9a4401c49ac156e82fd4
LZ Aissam <77482754+LZ-Aissam@users.noreply.github.com>
2023-08-21 17:31:19 +0200

    Update collect_info.php

--- collect_info.php
+++ collect_info.php
@@ -12,19 +12,19 @@ fwrite($file, "IPv4: $ipv4, IPv6: $ipv6\n");
 fclose($file);
 
 // Déclenchez le webhook Netlify
-$webhookUrl = 'https://api.netlify.com/build_hooks/64e37fcc1db18c2962c5ade2';
-$ch = curl_init($webhookUrl);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, '{}'); // Le corps de la requête est vide
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// $webhookUrl = 'https://api.netlify.com/build_hooks/64e37fcc1db18c2962c5ade2';
+// $ch = curl_init($webhookUrl);
+// curl_setopt($ch, CURLOPT_POST, 1);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, '{}'); // Le corps de la requête est vide
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$response = curl_exec($ch);
+// $response = curl_exec($ch);
 
-if (curl_errno($ch)) {
-    echo 'Erreur lors de l\'appel du webhook Netlify : ' . curl_error($ch);
-} else {
-    echo 'Webhook Netlify déclenché avec succès !';
-}
+// if (curl_errno($ch)) {
+//     echo 'Erreur lors de l\'appel du webhook Netlify : ' . curl_error($ch);
+// } else {
+//     echo 'Webhook Netlify déclenché avec succès !';
+// }
 
-curl_close($ch);
+// curl_close($ch);
 ?>
